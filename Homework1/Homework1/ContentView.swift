13dfb9360ecf91a1c89b2109c50b1d7315d0de87
Самарин Евгений <57903146+EvgeniiSamarin@users.noreply.github.com>
2021-09-20 22:48:36 +0300

    branch 1 (#1)
    * Update text
    
    * Update text color

--- Homework1/Homework1/ContentView.swift
+++ Homework1/Homework1/ContentView.swift
@@ -9,9 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-            .foregroundColor(.blue)
+        Text("Hello from branch 1")
+            .foregroundColor(.green)
     }
 }
 
