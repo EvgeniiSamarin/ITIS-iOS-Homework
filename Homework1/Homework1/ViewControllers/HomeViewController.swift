fa194ff797ead37d9c892c69843768f859251da7
EvgeniiSamarin <evgeniy.samarin@flatstack.com>
2021-09-20 22:29:52 +0300

    Add HomeViewController

---
+++ Homework1/Homework1/ViewControllers/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  Homework1
+//
+//  Created by Евгений Самарин on 20.09.2021.
+//
+
+import SwiftUI
+
+struct HomeViewController: View {
+
+    // MARK: - View
+
+    var body: some View {
+        Text("HomeViewController")
+    }
+}
+
+struct HomeViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewController()
+    }
+}
